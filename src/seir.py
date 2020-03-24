3882a86c0d4d891a59c5114ba348b9ab9c4b6e90
Kelly Pierce <kellyannepierce@gmail.com>
2020-03-24 18:32:48 -0500

    first commit

---
+++ src/seir.py
@@ -0,0 +1,132 @@
+import numpy as np
+import scipy
+from scipy import integrate
+import json
+import argparse
+import matplotlib.pyplot as plt
+import os
+
+# ----------------------------------------------------------------------------------------------
+# -- Read in parameters file
+# ----------------------------------------------------------------------------------------------
+
+def acquire_params(filename):
+
+    with open(filename, 'r') as param_file:
+        params = json.loads(param_file.read())
+
+    return params
+
+# ----------------------------------------------------------------------------------------------
+# -- Validate Inputs
+# ----------------------------------------------------------------------------------------------
+
+def validate_params(param_dict, float_keys, int_keys, str_keys):
+
+    all_keys = float_keys + int_keys + str_keys
+    for key in all_keys:
+        if key not in param_dict.keys():
+            raise ValueError('Parameter {} missing from input file.'.format(key))
+
+    for key in float_keys:
+        if type(param_dict[key]) != float:
+            raise ValueError('Parameter {} is not specified as a float.'.format(key))
+
+    for key in int_keys:
+        if type(param_dict[key]) != int:
+            raise ValueError('Parameter {} is not specified as an integer.'.format(key))
+
+    for key in str_keys:
+        if type(param_dict[key]) != str:
+            raise ValueError('Parameter {} is not specified as a string.'.format(key))
+
+
+# ----------------------------------------------------------------------------------------------
+# -- define the system of differential equations
+# ----------------------------------------------------------------------------------------------
+
+class SEIR:
+
+    def __init__(self, beta, mu, sigma, gamma, start_S, start_E, start_I, start_R, duration, outdir):
+
+        self.beta = beta
+        self.mu = mu
+        self.sigma = sigma
+        self.gamma = gamma
+        self.start_S = start_S
+        self.start_E = start_E
+        self.start_I = start_I
+        self.start_R = start_R
+        self.duration = duration
+        self.outdir = outdir
+        self.R = [self.start_S, self.start_E, self.start_I, self.start_R]
+
+    def seir(self, x, t):
+
+        S = x[0]
+        E = x[1]
+        I = x[2]
+        R = x[3]
+
+        y = np.zeros(4)
+
+        y[0] = self.mu - ((self.beta * I) + self.mu) * S
+        y[1] = self.beta * S * I - (self.mu + self.sigma) * E
+        y[2] = self.sigma * E - (self.mu + self.gamma) * I
+        y[3] = self.gamma * I - self.mu * R
+
+        return y
+
+    def integrate(self):
+
+        time = np.arange(0, self.duration, 0.01)
+        results = scipy.integrate.odeint(self.seir, self.R, time)
+
+        return results
+
+    def plot_timeseries(self, results):
+
+        time = np.arange(0, len(results[:, 1]))
+
+        plt.figure(figsize=(10,10), dpi=300)
+
+        plt.plot(
+            time, results[:, 0], "k",
+            time, results[:, 1], "g",
+            time, results[:, 2], "r",
+            time, results[:, 3], "b",)
+        plt.legend(("S", "E", "I", "R"), loc=0)
+        plt.ylabel("Population Size")
+        plt.xlabel("Time")
+        plt.xticks(rotation=45)
+        plt.title("SEIR Model")
+        plt.savefig(os.path.join(self.outdir, 'SEIR_Model.png'))
+        plt.show()
+
+def main(opts):
+
+    # ----- Load and validate parameters -----#
+
+    pars = acquire_params(opts.paramfile)
+
+    float_keys = ['beta', 'mu', 'sigma', 'gamma']
+    int_keys = ['start_S', 'start_E', 'start_I', 'start_R', 'duration']
+    str_keys = ['outdir']
+    validate_params(pars, float_keys, int_keys, str_keys)
+
+    # ----- Run model if inputs are valid -----#
+
+    seir_model = SEIR(**pars)
+    r = seir_model.integrate()
+    seir_model.plot_timeseries(r)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--paramfile', help='Path to parameters file.')
+
+    opts = parser.parse_args()
+
+    main(opts)
+
+
